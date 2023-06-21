93dece32ecd0515f2a6a59978c515f7ff8ac6c14
ngn <78868991+ngn13@users.noreply.github.com>
2023-06-21 16:17:36 +0300

    fixing colors

--- install.sh
+++ install.sh
@@ -35,31 +35,31 @@ npm i
 echo -e "$GREEN Done! $RESET"
 
 echo "{" > config.json
-echo "$BLUE Time to configure the server $RESET"
-read -p "$BLUE What port should the server listen on? $RESET" port 
+echo -e "$BLUE Time to configure the server $RESET"
+read -p "$(echo -e '$BLUE What port should the server listen on? $RESET')" port 
 echo '\t"port": $port,' >> config.json
-read -p "$BLUE What should be the max vault size? (0 for none) $RESET" max
+read -p "$(echo -e '$BLUE What should be the max vault size? \(0 for none\) $RESET')" max
 echo '\t"vault-max": $max,' >> config.json
-read -p "$BLUE What should the server password be? $RESET" pass
+read -p "$(echo -e '$BLUE What should the server password be? $RESET')" pass
 pass=$(echo -n $pass | sha512sum)
 pass=$(echo ${pass// /})
 pass=$(echo ${pass//-/})
 echo '\t"pass": "$pass"' >> config.json
 echo "}" >> config.json
-echo "$GREEN Configuration is done! $RESET"
+echo -e "$GREEN Configuration is done! $RESET"
 
-echo "$BLUE Installing pm2... $RESET"
+echo -e "$BLUE Installing pm2... $RESET"
 npm install pm2 -g 
-echo "$GREEN Done! $RESET"
-echo "$BLUE Starting the server... $RESET"
+echo -e "$GREEN Done! $RESET"
+echo -e "$BLUE Starting the server... $RESET"
 pm2 start index.js 
-echo "$GREEN Done! $RESET"
+echo -e "$GREEN Done! $RESET"
 
-echo "$GREEN Connect your server at: localhost:$port $RESET"
-echo "########################################################"
-echo "$GREEN To see the server status:$RESET pm2 list"
-echo "$GREEN To restart the server:$RESET pm2 restart <id>"
-echo "$GREEN To stop the server:$RESET pm2 stop <id>"
-echo "########################################################"
-echo "$GREEN Installation is completed"
-echo "$GREEN Thank you for using passctl, have a nice day! $RESET"
+echo -e "$GREEN Connect your server at: localhost:$port $RESET"
+echo -e "########################################################"
+echo -e "$GREEN To see the server status:$RESET pm2 list"
+echo -e "$GREEN To restart the server:$RESET pm2 restart <id>"
+echo -e "$GREEN To stop the server:$RESET pm2 stop <id>"
+echo -e "########################################################"
+echo -e "$GREEN Installation is completed"
+echo -e "$GREEN Thank you for using passctl, have a nice day! $RESET"
